433f90ab148426dc63a5e25ff8d7b2e62568a2c0
kut7728 <kut7728@hanyang.ac.kr>
2025-02-05 00:46:25 +0900

    [FEAT] 약속생성 3단계 뷰생성 애니메이션 구현

--- momogum/Appoint/View/NewAppoint/AppointCreate3View.swift
+++ momogum/Appoint/View/NewAppoint/AppointCreate3View.swift
@@ -10,8 +10,13 @@ import SwiftUI
 /// 약속생성의 3단계, 모임이름(appointName), 식사메뉴(menuName), 식사일정(pickedDate), 식사장소(placeName), 특별소식(note) 적는 뷰
 struct AppointCreate3View: View {
     @Environment(NewAppointViewModel.self) var appointViewModel
-    
     @Binding var path: [String]
+    
+    @State var menuShowing: Bool = false
+    @State var dateShowing: Bool = false
+    @State var placeShowing: Bool = false
+    @State var noteShowing: Bool = false
+    @State var buttonShowing: Bool = false
     
     var body: some View {
         @Bindable var viewModel = appointViewModel
@@ -26,10 +31,15 @@ struct AppointCreate3View: View {
                         
                         TextField("ex. 더술 출발, 돈까스 먹방", text: $viewModel.appointName)
                             .modifier(ApmTextFieldModifier())
+                            .onSubmit {
+                                withAnimation {
+                                    menuShowing = true
+                                }
+                            }
                     }
                     
                     
-                    if (viewModel.appointName != "") {
+                    if (menuShowing) {
                         VStack (spacing: 0) {
                             Text("식사 메뉴를 알려주세요.")
                                 .font(.mmg(.subheader3))
@@ -37,10 +47,16 @@ struct AppointCreate3View: View {
                             
                             TextField("ex. 더술 닭한마리, 투파피 파스타", text: $viewModel.menuName)
                                 .modifier(ApmTextFieldModifier())
+                                .onSubmit {
+                                    withAnimation {
+                                        dateShowing = true
+                                    }
+                                }
                         }
+                        
                     }
                     
-                    if (viewModel.menuName != "") {
+                    if (dateShowing) {
                         VStack (spacing: 20) {
                             Text("식사 일정을 알려주세요.")
                                 .font(.mmg(.subheader3))
@@ -53,13 +69,16 @@ struct AppointCreate3View: View {
                                 .environment(\.locale, Locale(identifier: String(Locale.preferredLanguages[0])))
                                 .background(.black_5)
                                 .clipShape(RoundedRectangle(cornerRadius: 12))
+                                .onAppear {
+                                    withAnimation {
+                                        placeShowing = true
+                                    }
+                                }
                             
                         }
-                        .padding(.bottom, 20)
-                        
                     }
                     
-                    if (viewModel.menuName != "") {
+                    if (placeShowing) {
                         VStack (spacing: 0) {
                             Text("어디서 만날까요?")
                                 .font(.mmg(.subheader3))
@@ -67,10 +86,16 @@ struct AppointCreate3View: View {
                             
                             TextField("ex. 중앙동 다이소 앞, 학교 쪽문 앞", text: $viewModel.placeName)
                                 .modifier(ApmTextFieldModifier())
+                                .onSubmit {
+                                    withAnimation {
+                                        noteShowing = true
+                                    }
+                                }
                         }
+                        
                     }
                     
-                    if (viewModel.placeName != "") {
+                    if (noteShowing) {
                         VStack (spacing: 0) {
                             Text("특별한 소식이 있나요?")
                                 .font(.mmg(.subheader3))
@@ -89,7 +114,7 @@ struct AppointCreate3View: View {
             
             
             /// 다음 호버 버튼
-            if (viewModel.note != "") {
+            if (noteShowing) {
                 VStack {
                     Spacer()
                     HStack {
