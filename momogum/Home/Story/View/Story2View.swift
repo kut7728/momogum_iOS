48ee3e6b66a78756730286bae96bbf9bbf18730e
dori <rladbswls_06@naver.com>
2025-02-05 12:50:54 +0900

    [CHORE] #54 스토리뷰 디자인 구현

--- momogum/Home/Story/View/Story2View.swift
+++ momogum/Home/Story/View/Story2View.swift
@@ -13,69 +13,99 @@ struct Story2View: View {
     @State private var showReportSheet = false // 신고하기 모달을 표시할 상태 변수
     
     var body: some View {
-        VStack {
-            Rectangle()
-                .frame(width:352, height:6)
-                .cornerRadius(10)
-                .foregroundStyle(.black_3)
-                .padding(.top, 8)
+        ZStack {
+            Color(.black_5)
+                .edgesIgnoringSafeArea(.all)
             
-            HStack {
-                Circle()
-                    .frame(width:64, height:64)
-                    .padding(.leading, 24)
-                    .padding(.top, 22)
-                    .foregroundColor(Color(red: 207 / 255, green: 207 / 255, blue: 207 / 255))
-                VStack{
-                    HStack{
-                        Text("유저아이디")
-                            .font(.mmg(.subheader4))
-                            .bold()
-                            .padding(.top, 22)
+            VStack {
+                Rectangle()
+                    .frame(width:352, height:6)
+                    .cornerRadius(10)
+                    .foregroundStyle(.black_3)
+                    .padding(.top, 8)
+                
+                HStack {
+                    Circle()
+                        .frame(width:64, height:64)
+                        .padding(.leading, 24)
+                        .padding(.top, 22)
+                        .foregroundColor(Color(red: 207 / 255, green: 207 / 255, blue: 207 / 255))
+                    VStack {
+                        HStack {
+                            Text("유저아이디")
+                                .font(.mmg(.subheader4))
+                                .bold()
+                                .padding(.top, 22)
+                                .padding(.leading, 12)
+
+                            Text("n분")
+                                .foregroundColor(.black_3)
+                                .padding(.top, 22)
+                                .padding(.leading, 12)
+
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                        Text("식당이름")
+                            .font(.mmg(.Caption3))
+                            .foregroundColor(.black_2)
                             .padding(.leading, 12)
                             .frame(maxWidth: .infinity, alignment: .leading)
-                        Text("n분")
-                            .padding(.top, 22)
-                    }
-                    Text("식당이름")
-                        .font(.mmg(.Caption3))
-                        .padding(.leading, 12)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-            
-                Image("exclamation")
-                    .resizable()
-                    .frame(width:30, height:30)
-                    .padding(.top, 22)
-                    .padding(.leading, 8)
-                    .onTapGesture {
-                        showReportSheet.toggle() // 모달 띄우기
                     }
                 
-                Button(action: {  // 이미지 클릭 시 홈으로 돌아가기
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                    Image("close_s")
+                    Image("exclamation")
                         .resizable()
-                        .frame(width:38, height:38)
+                        .frame(width:30, height:30)
                         .padding(.top, 22)
+                        .padding(.leading, 8)
+                        .onTapGesture {
+                            showReportSheet.toggle() // 모달 띄우기
+                        }
+                    
+                    Button(action: {  // 이미지 클릭 시 홈으로 돌아가기
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image("close_s")
+                            .resizable()
+                            .frame(width:38, height:38)
+                            .padding(.top, 22)
+                    }
+                    Spacer()
                 }
+                
+                ZStack {
+                    Rectangle() // 게시글 프레임
+                        .frame(width:360, height: 534)
+                        .foregroundColor(.white) // 배경색을 하얀색으로 설정
+                        .padding(.top, 44)
+
+                    VStack(alignment: .leading) {
+                        Image("post_image")
+                            .resizable()
+                            .frame(width: 328, height: 328)
+                            .clipped()
+                            .padding(.top, 35)
+                            .padding(.leading, 17)
+                        
+                        Text("진짜 최고로 맛있다...✨")
+                            .font(.mmg(.subheader3))
+                            .padding(.top, 32)
+                            .padding(.leading, 17)
+                        
+                        Spacer()
+                    }
+                    .frame(width: 360, height: 534, alignment: .topLeading)
+                }
+                
                 Spacer()
             }
         }
-        Rectangle() //게시글 프레임
-            .frame(width:360, height: 534)
-            .foregroundColor(Color(red: 247 / 255, green: 247 / 255, blue: 247 / 255))
-            .padding(.top, 44)
-        
-        Spacer()
-            .sheet(isPresented: $showReportSheet) {
-                ReportView()
-                    .presentationDetents([.height(560), .large])
-                    .presentationDragIndicator(.visible) 
-            }
-            .navigationBarBackButtonHidden(true) // 기본 백 버튼 숨기기
-
+        .sheet(isPresented: $showReportSheet) {
+            ReportView()
+                .presentationDetents([.height(560), .large])
+                .presentationDragIndicator(.visible)
+        }
+        .navigationBarBackButtonHidden(true) // 기본 백 버튼 숨기기
     }
 }
 
