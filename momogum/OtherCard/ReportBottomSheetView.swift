6f54e50cbdff900735ac060ec41b652d454d334a
seung yeon <137742302+moanuna@users.noreply.github.com>
2025-02-05 14:20:40 +0900

    [Feat] #58 신고하기 바텀 시트

---
+++ momogum/OtherCard/ReportBottomSheetView.swift
@@ -0,0 +1,87 @@
+//
+//  ReportBottomsheetView.swift
+//  momogum
+//
+//  Created by 조승연 on 2/5/25.
+//
+
+import SwiftUI
+
+struct ReportBottomSheet: View {
+    @Binding var isPresented: Bool
+    
+    let reasons = ["잘못된 정보", "상업적 광고", "음란물", "폭력성"]
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Capsule()
+                .frame(width: 40, height: 5)
+                .foregroundColor(.gray.opacity(0.5))
+                .padding(.top, 8)
+            
+            Text("신고하기")
+                .font(.system(size: 18, weight: .semibold))
+                .padding(.top, 24)
+
+            Divider()
+                .frame(width: 304.5, height: 0.5)
+                .background(Color.gray.opacity(0.5))
+                .padding(.top, 24)
+
+            Text("이 게시물을 신고하는 사유를 선택해주세요.")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.top, 40)
+
+            Text("신고는 익명으로 처리됩니다.")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.top, 20)
+
+            Divider()
+                .frame(width: 304.5, height: 0.5)
+                .background(Color.gray.opacity(0.5))
+                .padding(.top, 40)
+
+            
+            VStack(spacing: 0) {
+                ForEach(reasons, id: \.self) { reason in
+                    Button(action: {
+                        print("\(reason) 신고 선택됨")
+                        isPresented = false
+                    }) {
+                        HStack {
+                            Text(reason)
+                                .font(.system(size: 16))
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.leading, 40)
+
+                            Spacer()
+                        }
+                        .padding(.vertical, 20)
+                        .padding(.horizontal, 24)
+                        .background(Color.white)
+                    }
+                    
+                    Divider()
+                        .frame(width: 304.5, height: 0.5)
+                        .background(Color.gray.opacity(0.5))
+                }
+            }
+
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(90)
+        .shadow(radius: 0)
+        .ignoresSafeArea(edges: .bottom)
+    }
+}
+
+#Preview{
+    ReportBottomSheet(isPresented: .constant(false))
+}
