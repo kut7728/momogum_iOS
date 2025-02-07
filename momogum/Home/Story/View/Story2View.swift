a040eebd1914a2e9dc5236d164970fc360731422
dori <rladbswls_06@naver.com>
2025-02-07 17:12:50 +0900

    [CHORE] #54 스토리뷰 신고하기 모달창 수정

--- momogum/Home/Story/View/Story2View.swift
+++ momogum/Home/Story/View/Story2View.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct Story2View: View {
     var userID: String
     @Environment(\.presentationMode) var presentationMode // 뒤로가기 기능
-    @State private var showReportSheet = false // 신고하기 모달을 표시할 상태 변수
-    
+    @State private var showReportSheet = false // 신고 모달 상태
+    @State private var showPopup = false // 팝업 표시 상태
+
     var body: some View {
         ZStack {
             Color(.black_5)
@@ -42,7 +43,6 @@ struct Story2View: View {
                                 .foregroundColor(.black_3)
                                 .padding(.top, 22)
                                 .padding(.leading, 12)
-
                         }
                         .frame(maxWidth: .infinity, alignment: .leading)
 
@@ -52,16 +52,16 @@ struct Story2View: View {
                             .padding(.leading, 12)
                             .frame(maxWidth: .infinity, alignment: .leading)
                     }
-                
+
                     Image("exclamation")
                         .resizable()
                         .frame(width:30, height:30)
                         .padding(.top, 22)
                         .padding(.leading, 8)
                         .onTapGesture {
-                            showReportSheet.toggle() // 모달 띄우기
+                            showReportSheet.toggle() //모달
                         }
-                    
+
                     Button(action: {  // 이미지 클릭 시 홈으로 돌아가기
                         presentationMode.wrappedValue.dismiss()
                     }) {
@@ -76,7 +76,7 @@ struct Story2View: View {
                 ZStack {
                     Rectangle() // 게시글 프레임
                         .frame(width:360, height: 534)
-                        .foregroundColor(.white) // 배경색을 하얀색으로 설정
+                        .foregroundColor(.white)
                         .padding(.top, 44)
 
                     VStack(alignment: .leading) {
@@ -86,12 +86,12 @@ struct Story2View: View {
                             .clipped()
                             .padding(.top, 35)
                             .padding(.leading, 17)
-                        
+
                         Text("진짜 최고로 맛있다...✨")
                             .font(.mmg(.subheader3))
                             .padding(.top, 32)
                             .padding(.leading, 17)
-                        
+
                         Spacer()
                     }
                     .frame(width: 360, height: 534, alignment: .topLeading)
@@ -99,16 +99,43 @@ struct Story2View: View {
                 
                 Spacer()
             }
+
+            // 팝업 화면 중앙표시
+            if showPopup {
+                VStack {
+                    Text("신고가 접수되었습니다.\n검토는 최대 24시간 소요됩니다.")
+                        .font(.mmg(.Body3))
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 20)
+
+                    Rectangle()
+                        .frame(width: 300, height: 1)
+                        .foregroundColor(.gray)
+                        .padding(.top, 20)
+
+                    Button(action: {
+                        showPopup = false
+                    }) {
+                        Text("확인")
+                            .font(.mmg(.subheader4))
+                            .foregroundColor(.blue)
+                            .frame(maxWidth: .infinity, maxHeight: 44)
+                            .background(Color.white)
+                    }
+                }
+                .frame(width: 319, height: 185)
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+            }
         }
         .sheet(isPresented: $showReportSheet) {
-            ReportView()
-                .presentationDetents([.height(560), .large])
-                .presentationDragIndicator(.visible)
+            ReportView(showReportSheet: $showReportSheet, showPopup: $showPopup)
         }
-        .navigationBarBackButtonHidden(true) // 기본 백 버튼 숨기기
+        .navigationBarBackButtonHidden(true)
     }
-}
-
-#Preview {
-    Story2View(userID: "유저아이디")
 }
