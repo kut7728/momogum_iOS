7e2d19b7f78c088e93b61edd674b4a4f24f7a464
seung yeon <137742302+moanuna@users.noreply.github.com>
2025-02-16 16:34:21 +0900

    [Feat] #90 유저프로필 API 연동

--- momogum/Card/UserInfo/UserInfoView.swift
+++ momogum/Card/UserInfo/UserInfoView.swift
@@ -8,24 +8,35 @@
 import SwiftUI
 
 struct UserInfoView: View {
+    @ObservedObject var viewModel: MyCardViewModel
+
     var body: some View {
         HStack(spacing: 12) {
-            Circle()
-                .frame(width: 48, height: 48)
-                .foregroundColor(.gray)
-            
+            if let profileImageUrl = viewModel.myCard.userProfileImageLink, let url = URL(string: profileImageUrl) {
+                AsyncImage(url: url) { image in
+                    image.resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 48, height: 48)
+                        .clipShape(Circle())
+                } placeholder: {
+                    Circle()
+                        .frame(width: 48, height: 48)
+                        .foregroundColor(.gray)
+                }
+            } else {
+                Circle()
+                    .frame(width: 48, height: 48)
+                    .foregroundColor(.gray)
+            }
+
             VStack(alignment: .leading, spacing: 4) {
-                Text("유저아이디")
+                Text(viewModel.myCard.nickname)
                     .font(.system(size: 16, weight: .bold))
-                Text("포스팅 날짜")
+                Text(viewModel.myCard.mealDiaryCreatedAt)
                     .font(.system(size: 13))
                     .foregroundColor(.gray)
             }
             Spacer()
         }
     }
-}
-
-#Preview {
-    UserInfoView()
 }
