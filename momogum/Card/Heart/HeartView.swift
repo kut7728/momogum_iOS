832ed3f390de2257c2ca8242330d919de1c3e504
seung yeon <137742302+moanuna@users.noreply.github.com>
2025-02-16 16:45:20 +0900

    [Feat] #90 좋아요 API 연동

--- momogum/Card/Heart/HeartView.swift
+++ momogum/Card/Heart/HeartView.swift
@@ -8,30 +8,24 @@
 import SwiftUI
 
 struct HeartView: View {
-    @Binding var likeCount: Int
-    @State private var isLiked = false
+    @ObservedObject var viewModel: MyCardViewModel
 
     var body: some View {
         HStack {
             Button(action: {
-                isLiked.toggle()
-                likeCount += isLiked ? 1 : -1
+                viewModel.toggleLike()
             }) {
-                Image(isLiked ? "heart_fill" : "heart")
+                Image(viewModel.myCard.isLiked ? "heart_fill" : "heart")
                     .resizable()
                     .frame(width: 24, height: 24)
             }
-            
+
             Spacer().frame(width: 12)
 
-            Text("\(likeCount)")
+            Text(viewModel.myCard.likeCount >= 99 ? "99+" : "\(viewModel.myCard.likeCount)")
                 .font(.system(size: 16))
-                .opacity(likeCount > 0 ? 1 : 0)
+                .opacity(viewModel.myCard.likeCount > 0 ? 1 : 0)
         }
-        .frame(minWidth: 50) 
+        .frame(minWidth: 50)
     }
-}
-
-#Preview {
-    HeartView(likeCount: .constant(0))
 }
