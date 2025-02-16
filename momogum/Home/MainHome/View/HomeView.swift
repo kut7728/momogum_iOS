aa3e646c68174ec986a9f1fb0d5efb7e0ba6f22d
dori <rladbswls_06@naver.com>
2025-02-16 15:42:45 +0900

    [feat] #85 메인페이지 둘러보기 연동관련

--- momogum/Home/MainHome/View/HomeView.swift
+++ momogum/Home/MainHome/View/HomeView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 struct HomeView: View {
     @Binding var tabIndex: Int
     @Binding var isTabBarHidden: Bool
-    @StateObject private var viewModel = HomeViewModel()
+    @StateObject private var homeviewModel = HomeViewModel()
+    @StateObject private var mealDiaryViewModel = MealDiaryViewModel()
     @State private var path = NavigationPath() // 네비게이션 경로 추가
-
+    
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
     let normalButtonColor = Color(.black_5)
     let selectedButtonColor = Color(.Red_2)
-
+    
     var body: some View {
         NavigationStack(path: $path) {
             VStack {
@@ -26,11 +27,11 @@ struct HomeView: View {
                 Spacer().frame(height: 60)
                 categoryTitle()
                 categoryButtons()
-
-                if let _ = viewModel.selectedButtonIndex {
+                
+                if let _ = homeviewModel.selectedButtonIndex {
                     foodDiaryGridView()
                 }
-
+                
                 Spacer()
             }
             
@@ -60,28 +61,28 @@ extension HomeView {
                 .frame(width: 107, height: 37)
                 .padding(.leading, 20)
                 .padding(.top, 20)
-
+            
             Spacer()
             
             NavigationLink(destination: SearchView().onAppear { isTabBarHidden = true }
                 .onDisappear { isTabBarHidden = false }) {
-                Image(systemName: "magnifyingglass")
-                    .imageScale(.large)
-                    .foregroundStyle(.black)
-                    .padding(.trailing, 12)
-                    .padding(.top, 20)
-            }
-
+                    Image(systemName: "magnifyingglass")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 18)
+                        .padding(.top, 20)
+                }
+            
             NavigationLink(destination: BellView().onAppear { isTabBarHidden = true }
                 .onDisappear { isTabBarHidden = false }) {
-                Image(systemName: "bell")
-                    .imageScale(.large)
-                    .foregroundStyle(.black)
-                    .padding(.trailing, 27)
-                    .padding(.top, 20)
-            }
-
-            if viewModel.unreadNotificationCount > 0 {
+                    Image(systemName: "bell")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 20)
+                        .padding(.top, 20)
+                }
+            
+            if homeviewModel.unreadNotificationCount > 0 {
                 Circle()
                     .fill(Color.red)
                     .frame(width: 6, height: 6)
@@ -90,7 +91,7 @@ extension HomeView {
         }
         .padding(.horizontal, 16)
     }
-
+    
     // 스토리 리스트
     private func storyScrollView() -> some View {
         ScrollView(.horizontal, showsIndicators: false) {
@@ -100,8 +101,8 @@ extension HomeView {
             }
         }
     }
-
-
+    
+    
     private func storyItem(title: String, hasStory: Bool) -> some View {
         VStack {
             Button(action: {
@@ -124,7 +125,7 @@ extension HomeView {
                             .strokeBorder(Color.gray.opacity(0.5), lineWidth: 6)
                             .frame(width: 90, height: 90)
                     }
-
+                    
                     Image("pixelsImage")
                         .resizable()
                         .frame(width: 76, height: 76)
@@ -136,8 +137,8 @@ extension HomeView {
         }
         .padding(.leading, 24)
     }
-
-
+    
+    
     private func categoryTitle() -> some View {
         HStack {
             Text("다양한 밥일기 \n둘러보기")
@@ -147,18 +148,18 @@ extension HomeView {
             Spacer()
         }
     }
-
+    
     private func categoryButtons() -> some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 20) {
-                ForEach(viewModel.buttonLabels.indices, id: \.self) { index in
-                    Button(action: { viewModel.selectButton(index: index) }) {
-                        Text(viewModel.buttonLabels[index])
+                ForEach(homeviewModel.buttonLabels.indices, id: \.self) { index in
+                    Button(action: { homeviewModel.selectButton(index: index, mealDiaryViewModel: mealDiaryViewModel) }) {
+                        Text(homeviewModel.buttonLabels[index])
                             .font(.mmg(.subheader4))
                             .padding(.horizontal, 16)
                             .frame(height: 32)
-                            .background(viewModel.selectedButtonIndex == index ? selectedButtonColor : normalButtonColor)
-                            .foregroundColor(viewModel.selectedButtonIndex == index ? .white : .gray)
+                            .background(homeviewModel.selectedButtonIndex == index ? selectedButtonColor : normalButtonColor)
+                            .foregroundColor(homeviewModel.selectedButtonIndex == index ? .white : .gray)
                             .bold()
                             .cornerRadius(10)
                     }
@@ -168,30 +169,50 @@ extension HomeView {
             .padding(.leading, 12)
         }
     }
-
+    
     private func foodDiaryGridView() -> some View {
         ScrollView(.vertical, showsIndicators: false) {
             LazyVGrid(columns: columns, spacing: 16) {
-                ForEach(0..<6) { index in
+                ForEach(mealDiaryViewModel.mealDiaries) { diary in //`mealDiaries`를 반복
                     NavigationLink(destination: OtherCardView(isTabBarHidden: $isTabBarHidden)) {
                         VStack(spacing: 0) {
-                            Rectangle()
-                                .fill(Color.gray)
+                            ZStack(alignment: .topLeading) {
+                                AsyncImage(url: URL(string: diary.foodImageURLs.first ?? "")) { image in
+                                    image.resizable().scaledToFit()
+                                } placeholder: {
+                                    Image("post_image")
+                                        .resizable()
+                                        .frame(width: 166, height: 166)
+                                }
                                 .frame(width: 166, height: 166)
-
+                                
+                                if homeviewModel.selectedButtonIndex == 0 { // "또 올래요:)" 만 이미지 띄우기
+                                    Image("good_fill")
+                                        .resizable()
+                                        .frame(width: 36, height: 36)
+                                        .padding(.top, 123)
+                                        .padding(.leading, 122)
+                                }
+                            }
+                            
                             ZStack {
                                 Color.white
                                     .frame(width: 166, height: 75)
-
+                                
                                 HStack(spacing: 8) {
-                                    Circle()
-                                        .fill(Color.gray)
-                                        .frame(width: 36, height: 36)
-
-                                    Text("식사메뉴")
+                                    AsyncImage(url: URL(string: diary.userImageURL)) { image in
+                                        image.resizable().scaledToFill()
+                                    } placeholder: {
+                                        Image("pixelsImage")
+                                            .resizable()
+                                    }
+                                    .frame(width: 36, height: 36)
+                                    .clipShape(Circle())
+                                    
+                                    Text(diary.foodCategory)
                                         .font(.mmg(.Caption1))
                                         .foregroundColor(.black)
-
+                                    
                                     Spacer()
                                 }
                                 .frame(width: 144, height: 36)
@@ -201,7 +222,7 @@ extension HomeView {
                         .cornerRadius(10)
                         .overlay(
                             RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.gray, lineWidth: 1)
+                                .stroke(Color.black_4, lineWidth: 1)
                         )
                     }
                 }
@@ -209,6 +230,7 @@ extension HomeView {
             .padding(.horizontal, 16)
         }
     }
+    
 }
 
 
