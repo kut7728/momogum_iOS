ce0d1b1d8be7f72c3174f468444a6098a17c2428
dori <rladbswls_06@naver.com>
2025-02-17 18:03:41 +0900

    [CHORE] #100 무한 스크롤 추가 및  기타 수정

--- momogum/Home/MainHome/ViewModel/HomeViewModel.swift
+++ momogum/Home/MainHome/ViewModel/HomeViewModel.swift
@@ -8,51 +8,32 @@
 import SwiftUI
 
 class HomeViewModel: ObservableObject {
-    // UI 상태 변수
     @Published var selectedButtonIndex: Int? = nil
     @Published var userInput: String = ""
     @Published var unreadNotificationCount: Int = 2
-    @Published var selectedCategory: String? = nil // 선택카테고리 값 저장
-    
-    // 필터 버튼 데이터
-    let buttonLabels: [String] = [
-        "또올래요:)", "한식", "중식", "일식", "양식",
-        "아시안", "패스트푸드", "카페", "기타"
-    ]
-    
+    @Published var selectedCategory: FoodCategory? = nil // FoodCategory enum 타입으로 변경
+
+    let buttonLabels: [String] = ["또올래요:)"] + FoodCategory.allCases.map { $0.label }
+
     // 버튼 클릭 시 선택 상태 변경 + API 요청
     func selectButton(index: Int, mealDiaryViewModel: MealDiaryViewModel) {
         selectedButtonIndex = index
         let selectedLabel = buttonLabels[index]
 
-        // API에서 요구하는 카테고리 값으로 변환
-        let category = convertToAPIParameter(selectedLabel)
-        selectedCategory = category
-
         if selectedLabel == "또올래요:)" {
-            mealDiaryViewModel.fetchMealDiaries() 
+            selectedCategory = nil
+            mealDiaryViewModel.resetData(category: selectedCategory) // 새로운 카테고리 선택 시 처음부터 불러오기
         } else {
-            mealDiaryViewModel.fetchMealDiaries(category: category)
+            if let category = FoodCategory.fromLabel(selectedLabel) {
+                selectedCategory = category
+                mealDiaryViewModel.resetData(category: category) // 새로운 카테고리로 데이터 리셋
+            }
         }
     }
-    
+
+
     // 알림 카운트 감소 로직
     func clearNotifications() {
         unreadNotificationCount = 0
     }
-    
-    private func convertToAPIParameter(_ label: String) -> String {
-        switch label {
-        case "한식": return "KOREA"
-        case "중식": return "CHINA"
-        case "일식": return "JAPAN"
-        case "양식": return "WESTERN"
-        case "아시안": return "ASIAN"
-        case "패스트푸드": return "FASTFOOD"
-        case "카페": return "CAFE"
-        default: return "ETC"
-        }
-    }
 }
-
-
