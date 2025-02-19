11e39ac0bda48947fefe9f2245d7fb23039830db
dori <rladbswls_06@naver.com>
2025-02-19 17:40:45 +0900

    [CHORE] #112 모델파일 변경

--- momogum/Home/Search/Model/AccountSearchModel.swift
+++ momogum/Home/Search/Model/AccountSearchModel.swift
@@ -3,10 +3,11 @@
 //  momogum
 //
 //  Created by 김윤진 on 2/9/25.
-
+//
 
 import Foundation
 
+// API 응답 모델
 struct AccountSearchAPIResponse: Codable {
     let isSuccess: Bool
     let code: String
@@ -14,13 +15,19 @@ struct AccountSearchAPIResponse: Codable {
     let result: [AccountSearchResult]
 }
 
-struct AccountSearchResult: Codable, Identifiable {
+// 개별 계정 검색 결과 모델
+struct AccountSearchResult: Codable, Identifiable, Equatable {
     let id: Int
     let userName: String
     let userNickName: String
     let userImageURL: String
-    let searchFollowName: [String]
+    let searchFollowName: [String]?
     let searchFollowCount: Int
+    let hasStory: Bool
+    let hasViewedStory: Bool
+    let about: String  // 추가됨
+    let follower: Int  // 추가됨
+    let following: Int  // 추가됨
 
     enum CodingKeys: String, CodingKey {
         case id = "userId"
@@ -29,6 +36,16 @@ struct AccountSearchResult: Codable, Identifiable {
         case userImageURL
         case searchFollowName
         case searchFollowCount
+        case hasStory
+        case hasViewedStory
+        case about
+        case follower
+        case following
     }
 }
 
+// 검색 요청 모델
+struct AccountSearchRequest: Codable {
+    let query: String
+    let userId: Int
+}
