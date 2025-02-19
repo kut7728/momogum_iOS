54dd40a844c1cb20455905d077b6ca50e308f7b6
seung yeon <137742302+moanuna@users.noreply.github.com>
2025-02-19 15:29:03 +0900

    [Fix] #120 북마크 수정

--- momogum/Card/Bookmark/BookmarkView.swift
+++ momogum/Card/Bookmark/BookmarkView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct BookmarkView: View {
-    @Binding var showBookmark: Bool
     var viewModel: MyCardViewModel
     var mealDiaryId: Int
+    var onBookmarkToggled: () -> Void
 
     var body: some View {
         Button(action: {
+            let wasBookmarked = viewModel.myCard.showBookmark
             viewModel.toggleBookmarkAPI(mealDiaryId: mealDiaryId)
+
+            if !wasBookmarked {
+                onBookmarkToggled()
+            }
         }) {
             Image(viewModel.myCard.showBookmark ? "bookmark_fill" : "bookmark")
                 .resizable()
